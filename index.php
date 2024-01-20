be84d50e6b3894c0f8a1a5df0d5f0233f3c59d11
Elleined <demadegu@gmail.com>
2024-01-21 00:13:39 +0800

    added usage of namespace in all class

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
+
+use Elleined\PHPFileGenerator\CSVGenerator;
+use Elleined\PHPFileGenerator\EXCELGenerator;
+use Elleined\PHPFileGenerator\PDFGenerator;
+
 require_once "./vendor/autoload.php";
+require_once "./src/Generator.php";
+require_once "./src/CSVGenerator.php";
+require_once "./src/EXCELGenerator.php";
+require_once "./src/PDFGenerator.php";
+
+$pdfGenerator = new PDFGenerator();
+$pdfGenerator -> generate();
+
+$csvGenerator = new CSVGenerator();
+$csvGenerator -> generate();
+
+$excelGenerator = new EXCELGenerator();
+$excelGenerator -> generate();
