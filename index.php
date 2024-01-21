3e3965b58131a0ab8459d69e37f89e61f1fa7a62
Elleined <demadegu@gmail.com>
2024-01-21 23:46:19 +0800

    added fpdf dependency for pdf generation

--- index.php
+++ index.php
@@ -7,10 +7,15 @@ use Elleined\PHPFileGenerator\PDFGenerator;
 require_once "./vendor/autoload.php";
 require_once "./src/Generator.php";
 require_once "./src/CSVGenerator.php";
+require_once "./src/PDFGenerator.php";
 
-$csvGenerator = new CSVGenerator();
 $data = [
     ['John Smith', 'john@example.com', '555-555-1212'],
     ['Jane Doe', 'jane@example.com', '555-555-1213']
 ];
-$csvGenerator -> generate("my_first_generated_csv_file", $data);
+
+// $csvGenerator = new CSVGenerator();
+// $csvGenerator -> generate("my_first_generated_csv_file", $data);
+
+$pdfGenerator = new PDFGenerator();
+$pdfGenerator -> generate("my_first_generated_pdf_file", $data);
---
+++ src/PDFGenerator.php
@@ -0,0 +1,13 @@
+<?php
+namespace Elleined\PHPFileGenerator;
+
+use Elleined\PHPFileGenerator\Generator;
+
+class PDFGenerator implements Generator {
+    /**
+     * @overrides
+     */
+    public function generate(string $fileName, array $data): void {
+        
+    }
+}
